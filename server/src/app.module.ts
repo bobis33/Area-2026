531248cc4e72238e70f1c8ac6b33f11b136fed83
MASINA Elliot <elliot.masina@epitech.eu>
2025-11-13 12:18:18 +0100

    Feat: about.json route

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,14 @@ import { ConfigModule } from '@nestjs/config';
 
 import { Module } from '@nestjs/common';
 import { UserController } from '@controllers/user.controller';
+import { AboutController } from '@controllers/about.controller';
 import { UsersService } from '@services/user.service';
 import { PrismaService } from '@services/prisma.service';
-import {HealthController} from "@controllers/health.controller";
+import { HealthController } from '@controllers/health.controller';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true })],
-    controllers: [UserController, HealthController],
-    providers: [PrismaService, UsersService],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [HealthController, AboutController, UserController],
+  providers: [PrismaService, UsersService],
 })
 export class AppModule {}
