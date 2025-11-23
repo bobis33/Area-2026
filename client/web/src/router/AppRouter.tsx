cffc1eee814a885eb1874821233c874c5f3d656d
Arthur Comet <arthurcom@outlook.fr>
2025-11-23 17:54:29 +0100

    Fix: Router configuration with Home as default route

--- client/web/src/router/AppRouter.tsx
+++ client/web/src/router/AppRouter.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 
 /**
@@ -9,9 +10,9 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/about" replace />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<Navigate to="/about" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
