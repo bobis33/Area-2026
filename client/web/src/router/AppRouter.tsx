ea95a6e20e480137f8946b71bf2b1f725d21f957
Arthur Comet <arthurcom@outlook.fr>
2025-11-29 17:23:13 +0100

    Update: router with authentication routes

--- client/web/src/router/AppRouter.tsx
+++ client/web/src/router/AppRouter.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "@/pages/Home/Home";
 import About from "@/pages/About/About";
+import Login from "@/pages/Auth/Login";
 import Register from "@/pages/Auth/Register";
+import OAuthCallback from "@/pages/Auth/OAuthCallback";
+import OAuthError from "@/pages/Auth/OAuthError";
 
 /**
  * Main application router
@@ -11,9 +14,12 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/auth/success" element={<OAuthCallback />} />
+        <Route path="/auth/error" element={<OAuthError />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
