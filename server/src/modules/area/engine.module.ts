fe718bfd465826c5ea181b490603059252552d5b
bobi <masina.elliot@hotmail.fr>
2025-12-12 15:15:36 +0100

    Fix: paths

--- server/src/modules/area/engine.module.ts
+++ server/src/modules/area/engine.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { EngineService } from '@modules/area/engine.service';
 import { PrismaService } from '@common/database/prisma.service';
 import { DiscordModule } from '@modules/discord/discord.module';
-import { TimeEveryHandler } from '@modules/actions/time.handler';
-import { DiscordSendMessageReaction } from '@modules/reactions/send-message.reaction';
+import { TimeCronAction } from '@modules/actions/time/cron';
+import { DiscordSendMessageReaction } from '@modules/reactions/discord/send-message';
 
 @Module({
     imports: [DiscordModule],
     providers: [
         PrismaService,
         EngineService,
-        TimeEveryHandler,
+        TimeCronAction,
         DiscordSendMessageReaction
     ],
 })
