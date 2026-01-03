26fd9654eef246177a20a006c74bb1485b2d3aca
MASINA Elliot <elliot.masina@epitech.eu>
2026-01-03 10:16:49 +0100

    CI: build and run test

--- packages/server/test/app.e2e-spec.ts
+++ packages/server/test/app.e2e-spec.ts
@@ -5,13 +5,21 @@ import { App } from 'supertest/types';
 import { AppModule } from '@/app.module';
 import { PrismaService } from '@/common/database/prisma.service';
 
+import { DiscordService } from '@modules/discord/discord.service';
+
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(DiscordService)
+      .useValue({
+        onModuleInit: jest.fn(),
+        client: { login: jest.fn() },
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
